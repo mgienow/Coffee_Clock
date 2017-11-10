e22f904cc819b08879fe44ecf253129451bc6a3f
mgienow <michellegienow@gmail.com>
2017-11-10 15:18:26 -0500

    refactored with innerHTML

--- client.js
+++ client.js
@@ -1,52 +1,37 @@
-const today = new Date();
-const hourNow = today.getHours();
+const date = new Date();
+const hourNow = date.getHours();
+const minutesNow = date.getMinutes()
 
-const java = document.getElementById("java");
+const java = document.getElementById('java')
 
-// Create a variable called msg to hold a new message
-let msg = `It's Coffee O'Clock`;
+let msg = `It's Coffee O'Clock!`
+java.textContent = msg;
 
-// Create a function to update the content of the element whose id attribute has a value of "message"
-function updateMessage() {
-  var el = document.getElementById('java');
-  el.textContent = msg;
+let minutes = balancedMinutes(minutesNow)
+function balancedMinutes(i) {
+  if(i<10) {i='0'+i}
+  return i;
 }
 
-// Call the function
-updateMessage();
-
+const clock = document.getElementById('clock')
+clock.textContent = `${hourNow}:${minutes}`
 
 function sendSalutations() {
-  const salutations = document.getElementById("salutations");
-
-let greeting;
-if (hourNow > 18) {
-  greeting = `Good Evening!
-              How about some decaf?`;
-} else if (hourNow > 12) {
-  greeting = `Good afternoon!
-              An energizing espresso?`;
-} else if (hourNow > 0) {
-  greeting = `Good Morning!
-              Ahhh, that first cup...`;
-} else {
-  greeting = `Hello there!`;
-}
-
- salutations.textContent = greeting;
+  let coffeeBreak
+  if (hourNow>16){
+    coffeeBreak = `Good evening!<br>
+                    Like some decaf?`
+  } else if (hourNow>11){
+  coffeeBreak = `Good afternoon.<br>
+                How about an energizing<br>
+                espresso?`
+  }else if (hourNow>5){
+    coffeeBreak = `Good morning!<br>
+                    Ahhh, nothing like that first cup...`
+  } else {
+    coffeeBreak = `How do you take YOURS?`
+  }
+  const salutations = document.getElementById('salutations')
+  salutations.innerHTML = coffeeBreak
 }
 sendSalutations();
-
-function startTime() {
-    // let h = today.getHours();
-    let minutes = today.getMinutes();
-    minutes = checkTime(minutes);
-
-    document.getElementById('clock').innerHTML =
-    `${hourNow}:${minutes}`;
-    let t = setTimeout(startTime, 500);
-}
-function checkTime(i) {
-    if (i < 10) {i = '0' + i};  // add zero in front of numbers < 10
-    return i;
-}
